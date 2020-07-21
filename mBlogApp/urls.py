26c08bf3341927f198dcc177fb659e1c5b190e04
Kartik Ramesh <kartikx2000@gmail.com>
2020-07-21 10:09:06 +0530

    Login path

--- mBlogApp/urls.py
+++ mBlogApp/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('register', views.register, name='register'),
-    # path('login', views.login, name='login'),
+    path('login', views.login, name='login'),
 ]
