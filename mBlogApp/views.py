a6dc4b05609b21c3fabe70e6445bc46aca057b30
Kartik Ramesh <kartikx2000@gmail.com>
2020-07-22 23:55:53 +0530

    Imports

--- mBlogApp/views.py
+++ mBlogApp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.models import User
 from django.contrib import messages
 
-from .forms import RegistrationForm, LoginForm
+from .forms import RegistrationForm, LoginForm, CreatePostForm
+from .models import Post
 
 def welcome(request):
     return render(request, 'welcome.html')
