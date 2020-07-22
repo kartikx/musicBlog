927acb18309db6302966b56cd903ffbaa8e70681
Kartik Ramesh <kartikx2000@gmail.com>
2020-07-22 23:55:45 +0530

    Feed route

--- mBlogApp/urls.py
+++ mBlogApp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('register', views.register, name='register'),
     path('login', views.login, name='login'),
+    path('feed', views.feed, name='feed'),
 ]
