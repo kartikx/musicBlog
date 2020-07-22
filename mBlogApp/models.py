cca5cc256e11518e82b8986a5bfe70f2e1f93194
Kartik Ramesh <kartikx2000@gmail.com>
2020-07-22 23:55:35 +0530

    set date_posted to be Datetime instead of just Date

--- mBlogApp/models.py
+++ mBlogApp/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-# Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length= 50, blank= False)
     artist = models.CharField(max_length= 50, blank= False)
     content = models.TextField(blank= True, null= True)
-    date_posted = models.DateField(blank= False, default= timezone.now)
+    date_posted = models.DateTimeField(blank= False, default= timezone.now)
     author = models.ForeignKey(User, on_delete= models.SET_NULL, blank= True, null= True)
 
     def __str__(self):
