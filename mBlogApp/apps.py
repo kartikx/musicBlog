fd45d4f8b960ec17a3c0b724ad61322a86a61d33
Kartik Ramesh <kartikx2000@gmail.com>
2020-07-23 14:56:48 +0530

    I'm instantiating the SpotifyClient in AppConfig. This ensures that instantiation happens only once. This might not be the best practice however. Should Read up.

--- mBlogApp/apps.py
+++ mBlogApp/apps.py
@@ -1,8 +1,13 @@
 from django.apps import AppConfig
+from .spotifyclient import SpotifyClient
 
 
 class MblogappConfig(AppConfig):
     name = 'mBlogApp'
+    client_id = "11c45ddf7bc044b6b6ebff5cf3fc77cd"
+    client_secret = "ab374863a10240ab835146fc21d71205"
+
+    client = SpotifyClient(client_id = client_id, client_secret= client_secret)
 
     def ready(self):
         import mBlogApp.signals
