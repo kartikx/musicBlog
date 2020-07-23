528c6ee56ba2ec01aeff5fa683590e87257ef0d0
Kartik Ramesh <kartikx2000@gmail.com>
2020-07-23 14:53:02 +0530

    Spotify Client class

--- mBlogApp/spotifyclient.py
+++ mBlogApp/spotifyclient.py
@@ -3,3 +3,17 @@ import base64
 import datetime
 from urllib.parse import urlencode
 
+"""
+I'll instantiate this class in a global variable,
+which persists accross multiple View requests.
+This has the benefit that I won't need to make
+multiple authorization calls. 
+Each instance will serve for 60 minutes before
+needing to be authorized again.
+"""
+class SpotifyClient:
+    client_id = None
+    client_secret = None
+    access_token = None
+    expires_at = None
+
