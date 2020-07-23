8093475cf6c512626adbc9af3341fbc4b47b8c3c
Kartik Ramesh <kartikx2000@gmail.com>
2020-07-23 14:51:25 +0530

    Logout Route

--- mBlogApp/views.py
+++ mBlogApp/views.py
@@ -64,39 +64,6 @@ def feed(request):
     form = CreatePostForm()
     return render(request, 'feed.html', {'posts': posts, 'form': form})
 
-def download_image(song, artist):
-    options = Options()
-    options.add_argument('--headless')
-    driver = webdriver.Firefox(firefox_options= options)
-
-    # Not resolving for space removal between words, Google handles that for you.
-    searchurl = f"https://www.google.com/search?q={song}+{artist}+album+cover&client=ubuntu&hs=USc&channel=fs&sxsrf=ALeKk01Y5nCWBeQ_Z7aJebp81Qz7Lpz0hg:1595422846626&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjbpKig9eDqAhXq6XMBHfoqC_0Q_AUoAXoECA8QAw&biw=1297&bih=645"
-
-    driver.get(searchurl)
-
-    first_image_url = driver.find_element_by_xpath('//div//div//div//div//div//div//div//div//div//div[1]//a[1]//div[1]//img[1]')
-    first_image_url.click()
-
-    time.sleep(3)
-
-    popup_image = driver.find_elements_by_class_name("n3VNCb")
-    popup_image_url = popup_image[0].get_attribute("src")
-
-    album_image = requests.get(popup_image_url)
-
-    song = ''.join(song.split(' '))
-    artist = ''.join(artist.split(' '))
-
-    filename = song + artist + '.' + popup_image_url[-3:]
-
-    if not os.path.exists('/home/kartik/musicBlog2.0/mBlogApp/static/AlbumArt'):
-        os.makedirs('/home/kartik/musicBlog2.0/mBlogApp/static/AlbumArt')
-
-    # optimize
-    image_path = os.path.join('/home/kartik/musicBlog2.0/mBlogApp/static/AlbumArt', filename)
-
-    if album_image.status_code == 200:
-        with open(image_path, 'wb') as f:
-            f.write(album_image.content) 
-
-    return filename
+def logout(request):
+    auth_logout(request)
+    return redirect('welcome')
