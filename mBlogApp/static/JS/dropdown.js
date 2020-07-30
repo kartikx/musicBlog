7b433f78ea3a010463b191486b06d9dd04c2abfb
Kartik Ramesh <kartikx2000@gmail.com>
2020-07-30 23:45:43 +0530

    Add hover for dropdown on entire div

--- mBlogApp/static/JS/dropdown.js
+++ mBlogApp/static/JS/dropdown.js
@@ -2,7 +2,7 @@ const profilephotodiv = document.querySelector(".profile__photo");
 const profilephoto = profilephotodiv.querySelector("img");
 const dropdownmenu = profilephotodiv.querySelector(".profile-dropdown");
 
-profilephoto.addEventListener("click", (_) => {
+profilephotodiv.addEventListener("click", (_) => {
     console.log('added');
     dropdownmenu.classList.toggle("dropdown-is-visible");
 });
