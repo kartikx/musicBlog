26fe275d47031a48574177b4126e99ba162faa80
Kartik Ramesh <kartikx2000@gmail.com>
2020-08-04 21:15:08 +0530

    Added STATIC_URL

--- musicBlog/settings.py
+++ musicBlog/settings.py
@@ -132,4 +132,5 @@ MESSAGE_TAGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
