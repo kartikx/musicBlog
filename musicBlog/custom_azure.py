03bc1d35ed9e73ea9e941961cf7b5299d2121949
Kartik Ramesh <kartikx2000@gmail.com>
2020-08-06 18:24:34 +0530

    polish

---
+++ musicBlog/custom_azure.py
@@ -0,0 +1,13 @@
+from storages.backends.azure_storage import AzureStorage
+import os
+class AzureMediaStorage(AzureStorage):
+    account_name = os.environ.get('AZURE_ACCOUNT_NAME')
+    account_key =  os.environ.get('AZURE_ACCOUNT_KEY')
+    azure_container = 'media'
+    expiration_secs = None
+
+class AzureStaticStorage(AzureStorage):
+    account_name = os.environ.get('AZURE_ACCOUNT_NAME')
+    account_key =  os.environ.get('AZURE_ACCOUNT_KEY')
+    azure_container = 'static'
+    expiration_secs = None
