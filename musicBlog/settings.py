a9af61ed2fd96f32155a9403025232280c639fe7
Kartik Ramesh <kartikx2000@gmail.com>
2020-08-06 18:25:17 +0530

    Add media url, commenting media root because using Online Hosting

--- musicBlog/settings.py
+++ musicBlog/settings.py
@@ -35,7 +35,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Application definition
 
