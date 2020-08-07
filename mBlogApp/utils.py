7b39a42c30d008cb9010851f7b62e93d6bfd5c08
Kartik Ramesh <kartikx2000@gmail.com>
2020-08-07 23:08:19 +0530

    Genre Download Functionality

--- mBlogApp/utils.py
+++ mBlogApp/utils.py
@@ -20,3 +20,14 @@ def get_random_alphanumeric_string(length):
     letters_and_digits = string.ascii_letters + string.digits
     result_string = ''.join((random.choice(letters_and_digits) for i in range(length)))
     return result_string
+
+def get_and_save_genre_desc(genre):
+    session = HTMLSession()
+    search_query = f"https://www.google.com/search?client=ubuntu&channel=fs&sxsrf=ALeKk00_1oJuZCQc9c9-BbFHMcyUJbxlMA%3A1596778189236&ei=zeYsX5z-DYia4-EP0KC7qAs&q=what+is+{genre.name}"
+
+    r = session.get(search_query)
+    descbox = r.html.find(".kno-rdesc")
+    span_items = descbox[0].find("span")
+
+    genre.desc = span_items[0].text
+    genre.save()
