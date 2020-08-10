ba87eaec5e64c313fca1242c714e905dbed7265a
Kartik Ramesh <kartikx2000@gmail.com>
2020-08-10 14:37:56 +0530

    Follow Medium Article for PostGres setup on Heroku and SQLite on Local.

--- musicBlog/settings.py
+++ musicBlog/settings.py
@@ -13,10 +13,15 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 from django.contrib.messages import constants as messages
+import dj_database_url
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -59,7 +64,7 @@ MIDDLEWARE = [
 ]
 
 MIDDLEWARE_CLASSES = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'musicBlog.urls'
@@ -89,12 +94,15 @@ WSGI_APPLICATION = 'musicBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
