d717e36122bc6a96bc8c52ca1d44dcfe384371b5
Kartik Ramesh <kartikx2000@gmail.com>
2020-08-10 12:45:19 +0530

    Add About, Explore, Community URLs as well as 500 Handler.

--- mBlogApp/urls.py
+++ mBlogApp/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
+    path('about/', views.about, name='about'),
+    path('explore/', views.explore, name='explore'),
+    path('community/', views.community, name='community'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('feed/', views.feed, name='feed'),
@@ -11,3 +14,5 @@ urlpatterns = [
     path('user/<str:username>/', views.profile, name='profile'),
     path('genre/<slug:genrename>/', views.genre, name='genre'),
 ]
+
+handler500 = views.handler500
