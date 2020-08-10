2ea0d6afee180152bb474ae91132406bca66412b
Kartik Ramesh <kartikx2000@gmail.com>
2020-08-10 12:45:37 +0530

    Important Imports to work with Azure Storage and Resize.

--- mBlogApp/models.py
+++ mBlogApp/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.core.files.storage import default_storage as storage
 
 from PIL import Image
+import io
 
 class Genre(models.Model):
     name = models.CharField(max_length=50, blank= True, null= True)
